99cc2a191de708b4981a0409aa7933b81d00dccf
admin <gali.naga0410@gmail.com>
2025-01-23 21:56:06 -0800

    created project

---
+++ Day04/java-exception/src/main/java/com/naga/app/custome/AgeValidationException.java
@@ -0,0 +1,7 @@
+package com.naga.app.custome;
+
+public class AgeValidationException extends Exception {
+    public AgeValidationException(String message) {
+        super(message);
+    }
+}
---
+++ Day04/java-exception/src/main/java/com/naga/app/custome/MyException.java
@@ -0,0 +1,8 @@
+package com.naga.app.custome;
+
+public class MyException extends Exception {
+    private static final long serialVersionUID = 1L;
+    public MyException(String message) {
+        super(message);
+    }
+}
