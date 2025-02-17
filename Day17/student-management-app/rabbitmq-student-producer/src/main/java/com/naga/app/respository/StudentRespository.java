76916e3a4b27a260c50836ab5ad6f3ba75b4d63b
admin <rama@gail.com>
2025-02-17 15:20:29 -0800

    Commit

---
+++ Day17/student-management-app/rabbitmq-student-producer/src/main/java/com/naga/app/respository/StudentRespository.java
@@ -0,0 +1,15 @@
+package com.naga.app.respository;
+
+import com.naga.app.model.Student;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface StudentRespository extends CrudRepository<Student, Integer> {
+    Map<String, Student> students = new HashMap<String, Student>();
+
+    public default void deleteName(String name) {
+        students.remove(name);
+    }
+}
