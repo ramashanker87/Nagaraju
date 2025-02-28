414ac9433c549c5604484d6a86fc4d706217e0e6
admin <rama@gail.com>
2025-02-27 22:51:51 -0800

    Initial Spring Boot application commit

---
+++ Day22/digital-parking-system/car-parking-registration/src/main/java/com/naga/app/CarParkingRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.naga.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarParkingRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalParkingSystemApplication.class, args);
+	}
+
+}
